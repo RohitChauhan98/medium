d6b24b0adec4baa25f4b90fb6b5c18feb2afaa35
Rohit Chauhan <rc56951@gmail.com>
2024-04-19 11:04:25 +0530

    fixed blank page Load

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,38 @@
 import './App.css'
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
 import {Signup} from './pages/Signup'
 import {Signin} from './pages/Signin'
 import {Blog} from './pages/Blog'
 import { Blogs } from './pages/Blogs' 
 import { Publish } from './pages/Publish' 
+import { useEffect } from 'react'
+
 
 function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
+
+
+export default App
+
+
+function AppContent() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      navigate('/signup');
+    } else {
+      navigate('/blogs');
+    }
+  }, []);
 
   return (
     <>
-      <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup/>} />
           <Route path="/signin" element={<Signin/>} />
@@ -18,10 +40,8 @@ function App() {
           <Route path="/blogs" element={<Blogs/>} />
           <Route path="/publish" element={<Publish/>} />
         </Routes>
-      </BrowserRouter>
+
     </>
      
   )
 }
-
-export default App
